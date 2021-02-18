e65ec80175fbc6697722c87cbbb4820bce0b5bb9
KoyanagiAyuha <ayuha.koyanagi@gmail.com>
2021-02-18 21:53:14 +0900

    copy

--- resultcopy.py
+++ resultcopy.py
@@ -22,8 +22,9 @@ if __name__ == "__main__":
         BUCKET_NAME = os.environ.get('BUCKET_NAME')
         SOURCE_DIR = os.environ.get('SOURCE_DIR')
         COPY_DIR = os.environ.get('COPY_DIR')
-        API_KEY = os.environ.get('API_KEY')
         API_ENDPOINT = os.environ.get('API_ENDPOINT')
+        SOURCE_FILE = os.environ.get('SOURCE_FILE')
+        COPY_FILE = os.environ.get('COPY_FILE')
         
         logging.critical('set_S3')
         s3 = boto3.client('s3')
@@ -37,20 +38,16 @@ if __name__ == "__main__":
 
         logging.critical('copy_json')
         for json_path in json_path_list:
-            copy_json_path = json_path.replace(SOURCE_DIR, COPY_DIR)
+            copy_path = json_path.replace(SOURCE_DIR, COPY_DIR)
+            copy_json_path = copy_path.replace(SOURCE_FILE, COPY_FILE)
             s3.copy_object(Bucket=BUCKET_NAME, Key=copy_json_path, CopySource={'Bucket': BUCKET_NAME, 'Key': json_path})
 
         logging.critical('copy_jpg')
         for jpg_path in jpg_path_list:
-            copy_jpg_path = jpg_path.replace(SOURCE_DIR, COPY_DIR)
+            copy_path = jpg_path.replace(SOURCE_DIR, COPY_DIR)
+            copy_jpg_path = copy_path.replace(SOURCE_FILE, COPY_FILE)
             s3.copy_object(Bucket=BUCKET_NAME, Key=copy_jpg_path, CopySource={'Bucket': BUCKET_NAME, 'Key': jpg_path})
 
-        filepath = COPY_DIR + '/json'
-        headers = {'Content-Type': 'application/json'}
-        seq_list = COPY_DIR.split('-')[1:]
-        seq = '-'.join(seq_list)
-        body = {'seq':seq, 'filepath':filepath, 'token':API_KEY}
-        logging.critical(body)
         exit(0)
 
     except Exception as e:
